bf41e62d4140bec0ec2a75d6d6f0ff4c0f40d29b
Mateusz Bartosiak <mateuszbartosiak@users.noreply.github.com>
2017-07-13 12:06:47 +0200

    Added register and login functionality

--- WebApplication1core/Controllers/AccountController.cs
+++ WebApplication1core/Controllers/AccountController.cs
@@ -49,29 +49,29 @@ namespace WebApplication1core.Controllers
 
 
 
-        //[HttpGet]
-        //public IActionResult Login()
-        //{
-        //    return View();
-        //}
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
 
-        //[HttpPost]
-        //public async Task<IActionResult> Login(LoginViewModel model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
-        //        if (result.Succeeded)
-        //        {
-        //            return RedirectToAction("Index", "Home");
-        //        }
-        //        else
-        //        {
-        //            ModelState.AddModelError(string.Empty,"Invalid credentials");
-        //        }
-        //    }
-        //    return View();
-        //}
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty,"Invalid credentials");
+                }
+            }
+            return View();
+        }
 
         [Authorize]
         public string Check()
